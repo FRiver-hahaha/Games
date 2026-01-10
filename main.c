2a5b7332ff9dc0423f1f529b271c5857957fc853
FRiver-hahaha <1203987745@qq.com>
2026-01-10 15:11:58 +0800

    猜数字游戏

--- main.c
+++ main.c
@@ -1,26 +1,124 @@
-// 2026.1.9
+// 2026.1.10
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <termios.h>
 
 #define COLOR_RESET "\033[0m" // 重置 
-#define COLOR_WELCOME "\033[1;34m" // 欢迎界面
-#define COLOR_DISPLAY "\033[1;33m" // 游戏界面
+#define COLOR_WELCOME "\033[1;34m" // 欢迎界面(粗体蓝色)
+#define COLOR_DISPLAY "\033[1;33m" // 游戏界面(粗体黄色)
+#define COLOR_ERROR "\033[1;31m" // 显示错误个数(粗体红色)
+#define COLOR_WIN "\033[1;36m" // 胜利界面(粗体青色)
 
 void FirstShow();
 void IntroduceGame();
 void ExitGame();
 void IntroduceAuthor();
+void StartGame();
+int SearchNumber(char [], char []);
+void Qsort(int arr[], int low, int high);
 
 int main() {
+    char ch;
     FirstShow();
-    printf("start!");
-
+    printf("start!\n");
+    sleep(1);
+    StartGame();
+    do {
+        printf("是否还要再来一轮？(按c继续，按q退出...)\n");
+        getchar();
+        ch = getchar();
+        if(ch == 'c') {
+            printf("start!\n");
+            sleep(1);
+            StartGame();
+        }
+    }while(ch != 'q'); 
+    ExitGame();
     exit(EXIT_SUCCESS);
+}
+
+void Qsort(int arr[], int low, int high) {
+    int i = low;
+    int j = high;
+    int key = arr[low];
+    if(i > j) {
+        return;
+    }
+    while(i < j) {
+        while(i < j && arr[j] >= key) {
+            j--;
+        }
+
+        while(i < j && arr[i] <= key) {
+            i++;
+        }
+
+        if(i < j) {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+    int tmp = arr[low];
+    arr[low] = arr[i];
+    arr[i] = tmp;
+    Qsort(arr, low, i - 1);
+    Qsort(arr, i + 1, high);
+}
+
+int SearchNumber(char nums[], char pnums[]) {
+    int cnt = 0;
+    for(int i = 0; i < 5; i++) {
+        if(nums[i] != pnums[i]) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+void StartGame() {
+    srand(time(NULL));
+    int cnt = 0;
+    int min = 10000, max = 99999;
+    int number = rand() % (max - min + 1) + min;
+    char nums[10], pnums[10];
+    int snums[5];
+    int tmp = number;
+    for(int i = 4; i >= 0; i--) {
+        nums[i] = '0' + (tmp % 10);
+        snums[i] = tmp % 10;
+        tmp /= 10;
+    }
+    nums[5] = '\0';
+    printf("已生成数字!\n");
+    Qsort(snums, 0, 4);
+    printf("当前可选数字有:");
+    for(int i = 0; i < 5; i++) {
+        printf("%d", snums[i]);
+        if(!(i == 5 - 1)) {
+            printf(" ");
+        }   
+    }
+    printf("\n");
+    sleep(1);
+    do {
+        printf("请输入你的答案：\n");
+        scanf("%s",pnums);
+        pnums[5] = '\0';
+        cnt = SearchNumber(nums, pnums);
+        if(cnt) {
+            printf("%s有%d个错误！%s\n",COLOR_ERROR, cnt, COLOR_RESET);
+        }
+    }while(cnt != 0);
+    printf("%s正确！%s\n",COLOR_WIN, COLOR_RESET);
+    return;
 }
 
 void IntroduceAuthor() {
@@ -57,6 +155,7 @@ void IntroduceGame() {
 
 void ExitGame() {
     printf("游戏已退出...");
+    printf("感谢您的游玩，我们下次再见...\n");
     exit(EXIT_SUCCESS);
 }
 

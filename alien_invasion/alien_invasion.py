50b0abb38c4f3246745038337cdf9164827c9d1b
FRiver-hahaha <1203987745@qq.com>
2026-04-16 21:06:32 +0800

    外星人入侵:完成飞船的绘制

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,14 @@
+# 2026.4.16 完成飞船的导入和绘制
+
+# 下次完整重构代码模块
+
 import sys
 
 import pygame
 
 from settings import Settings
+
+from ship import Ship
 
 def runGame() :
     # 创建屏幕对象，初始化设置
@@ -10,6 +16,8 @@ def runGame() :
     aiSettings = Settings()
     screen = pygame.display.set_mode((aiSettings.screenWidth, aiSettings.screenHeight))
     pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
 
     # 开始游戏的主循环
     while True:
@@ -21,6 +29,7 @@ def runGame() :
         
         # 填充背景颜色
         screen.fill(aiSettings.bgColor)
+        ship.blitme()
 
         pygame.display.flip()
 
